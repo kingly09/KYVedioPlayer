cc9275bcd6eb197ea0bc86bef13599c761d293ea
kingly <362108564@qq.com>
2016-09-08 17:02:07 +0800

    初始化工程 设置音频会话

---
+++ KYVedioPlayer/KYVedioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KYVedioPlayer
+//
+//  Created by kingly on 16/9/8.
+//  Copyright © 2016年 https://github.com/kingly09/KYVedioPlayer kingly  inc . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KYVedioPlayer/KYVedioPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KYVedioPlayer
+//
+//  Created by kingly on 16/9/8.
+//  Copyright © 2016年 https://github.com/kingly09/KYVedioPlayer kingly  inc . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
