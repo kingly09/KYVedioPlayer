bea62a0ffacecc063556b0745aa323fd2dbe62ff
kingly <kingly09@gmail.com>
2016-11-02 18:07:20 +0800

    提交

--- KYVedioPlayer/KYVedioPlayer/KYNetworkVideoCellPlayVC.m
+++ KYVedioPlayer/KYVedioPlayer/KYNetworkVideoCellPlayVC.m
@@ -59,12 +59,12 @@
  * 显示 从全屏来当前的cell视频
  **/
 -(void)showCellCurrentVedioPlayer{
-    
+
     if (currentVideo != nil &&  currentIndexPath != nil) {
-        
+
         KYNetworkVideoCell *currentCell = [self currentCell];
         [vedioPlayer removeFromSuperview];
-        
+
         [UIView animateWithDuration:0.5f animations:^{
             vedioPlayer.transform = CGAffineTransformIdentity;
             vedioPlayer.frame = currentCell.vedioBg.bounds;
@@ -104,7 +104,7 @@
             vedioPlayer.isFullscreen = NO;
             [self setNeedsStatusBarAppearanceUpdate];
             vedioPlayer.fullScreenBtn.selected = NO;
-            
+
         }];
     }
 }
@@ -137,44 +137,44 @@
     }
     NSString *path3gp = [[NSBundle mainBundle] pathForResource:@"贝加尔湖畔" ofType:@"3gp"];
     NSArray *arr = @[
-                    
+
                     @{@"title":@"视频一 mp4 格式",
                       @"image":@"http://vimg3.ws.126.net/image/snapshot/2016/9/L/1/VBVQVQRL1.jpg",
                       @"video":@"http://flv2.bn.netease.com/videolib3/1609/12/yRxoB7561/SD/yRxoB7561-mobile.mp4"},
-                    
+
                     @{@"title":@"视频二 m3u8格式",
                       @"image":@"http://vimg2.ws.126.net/image/snapshot/2016/9/7/7/VBV4B7Q77.jpg",
                       @"video":@"http://flv2.bn.netease.com/videolib3/1609/03/WotPc9077/SD/movie_index.m3u8"},
-                    
+
                     @{@"title":@"视频三 mov格式",
                       @"image":@"http://img2.cache.netease.com/m/3g/mengchong.png",
                       @"video":@"http://movies.apple.com/media/us/iphone/2010/tours/apple-iphone4-design_video-us-20100607_848x480.mov"},
-                    
+
                     @{@"title":@"视频四 3gp格式",
                       @"image":@"http://wimg.spriteapp.cn/picture/2016/0309/56df7b64b4416_wpd.jpg",
                       @"video":path3gp},
-                    
+
                     @{@"title":@"视频五",
                       @"image":@"http://vimg3.ws.126.net/image/snapshot/2016/9/A/G/VBV4BB5AG.jpg",
                       @"video":@"http://flv2.bn.netease.com/videolib3/1609/03/GVRLQ8933/SD/movie_index.m3u8"},
-                    
+
                     @{@"title":@"视频六",
                       @"image":@"http://vimg3.ws.126.net/image/snapshot/2016/9/5/1/VBV4BEH51.jpg",
                       @"video":@"http://flv2.bn.netease.com/videolib3/1609/03/lGPqA9142/SD/lGPqA9142-mobile.mp4"},
-                    
+
                     @{@"title":@"视频七",
                       @"image":@"http://vimg3.ws.126.net/image/snapshot/2016/9/U/R/VBVQV83UR.jpg",
                       @"video":@"http://flv2.bn.netease.com/videolib3/1609/12/aOzvT5225/HD/movie_index.m3u8"},
-                    
+
                     ];
-    
+
     NSMutableArray *arrVideo = [NSMutableArray array];
     for (NSDictionary *video in arr) {
         KYVideo *kYVideo = [[KYVideo alloc] init];
         kYVideo.title = [video objectForKey:@"title"];
         kYVideo.image = [video objectForKey:@"image"];
         kYVideo.video = [video objectForKey:@"video"];
-        
+
         [arrVideo addObject:kYVideo];
     }
     _dataSource = arrVideo;
@@ -200,12 +200,12 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  
+
     KYNetworkVideoCell *cell = [tableView dequeueReusableCellWithIdentifier:[KYNetworkVideoCell cellReuseIdentifier]];
     if (nil==cell)
     {
         cell = [[KYNetworkVideoCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:[KYNetworkVideoCell cellReuseIdentifier]];
-        
+
     }
     KYVideo *kYVideo  = self.dataSource[indexPath.row];
 
@@ -214,7 +214,7 @@
     cell.video = kYVideo;
     cell.mydelegate = self;
     cell.playBtn.tag = indexPath.row;
-    
+
     if (vedioPlayer && vedioPlayer.superview) {
         if (indexPath.row == currentIndexPath.row) {
             [cell.playBtn.superview sendSubviewToBack:cell.playBtn];    //隐藏播放按钮
@@ -223,7 +223,7 @@
         }
         NSArray *indexpaths = [tableView indexPathsForVisibleRows];
         if (![indexpaths containsObject:currentIndexPath] && currentIndexPath!=nil) { //复用机制
-            
+
             if ([[UIApplication sharedApplication].keyWindow.subviews containsObject:vedioPlayer]) {
                 vedioPlayer.hidden = NO;
             }else{
@@ -236,18 +236,18 @@
                 [vedioPlayer play];
                 vedioPlayer.hidden = NO;
             }
-            
+
         }
     }
 
-    
+
     return cell;
 }
 
 #pragma mark - UITableViewDelegate
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
+
     if (self.dataSource.count > 0) {
          KYVideo *kYVideo  = self.dataSource[indexPath.row];
          return kYVideo.curCellHeight;
@@ -265,7 +265,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
+
     KYLocalVideoPlayVC *localVideoPlayVC = [[KYLocalVideoPlayVC alloc] init];
     KYVideo *kYVideo  = self.dataSource[indexPath.row];
     localVideoPlayVC.title = kYVideo.title;
@@ -274,13 +274,13 @@
     backItem.title = @"返回";
     self.navigationItem.backBarButtonItem = backItem;
     [self.navigationController pushViewController:localVideoPlayVC animated:YES];
-   
+
 }
 
 #pragma mark - KYNetworkVideoCellDelegate
 
 -(void)networkVideoCellVedioBgTapGesture:(KYVideo *)video{
-    
+
     KYLocalVideoPlayVC *localVideoPlayVC = [[KYLocalVideoPlayVC alloc] init];
     localVideoPlayVC.title = video.title;
     localVideoPlayVC.URLString = video.video;
@@ -294,18 +294,18 @@
 -(void)networkVideoCellOnClickVideoPlay:(KYVideo *)video withVideoPlayBtn:(UIButton *)videoPlayBtn;{
 
     [self closeCurrentCellVedioPlayer];
-    
+
     currentVideo = video;
     currentIndexPath = [NSIndexPath indexPathForRow:videoPlayBtn.tag inSection:0];
     KYNetworkVideoCell *cell =nil;
     if ([UIDevice currentDevice].systemVersion.floatValue>=8||[UIDevice currentDevice].systemVersion.floatValue<7) {
         cell = (KYNetworkVideoCell *)videoPlayBtn.superview.superview;
-        
+
     }else{//ios7系统 UITableViewCell上多了一个层级UITableViewCellScrollView
         cell = (KYNetworkVideoCell *)videoPlayBtn.superview.superview.subviews;
-        
+
     }
-    
+
     if (vedioPlayer) {
         [self releasePlayer];
         vedioPlayer = [[KYVedioPlayer alloc]initWithFrame:cell.vedioBg.bounds];
@@ -314,14 +314,14 @@
         vedioPlayer.titleLabel.text = video.title;
         vedioPlayer.URLString = video.video;
     }else{
-        
+
         vedioPlayer = [[KYVedioPlayer alloc]initWithFrame:cell.vedioBg.bounds];
         vedioPlayer.delegate = self;
         vedioPlayer.closeBtnStyle = CloseBtnStyleClose;
         vedioPlayer.titleLabel.text = video.title;
         vedioPlayer.URLString = video.video;
     }
-    
+
     [cell.vedioBg addSubview:vedioPlayer];
     [cell.vedioBg bringSubviewToFront:vedioPlayer];
     [cell.playBtn.superview sendSubviewToBack:cell.playBtn];
@@ -335,7 +335,7 @@
 -(void)closeCurrentCellVedioPlayer{
 
     if (currentVideo != nil &&  currentIndexPath != nil) {
-        
+
         KYNetworkVideoCell *currentCell = [self currentCell];
         [currentCell.playBtn.superview bringSubviewToFront:currentCell.playBtn];
         [vedioPlayer removeFromSuperview];
@@ -347,19 +347,19 @@
 #pragma mark - KYVedioPlayerDelegate 播放器委托方法
 //点击播放暂停按钮代理方法
 -(void)kyvedioPlayer:(KYVedioPlayer *)kyvedioPlayer clickedPlayOrPauseButton:(UIButton *)playOrPauseBtn{
-    
+
     NSLog(@"[KYVedioPlayer] clickedPlayOrPauseButton ");
 }
 //点击关闭按钮代理方法
 -(void)kyvedioPlayer:(KYVedioPlayer *)kyvedioPlayer clickedCloseButton:(UIButton *)closeBtn{
-    
+
     NSLog(@"[KYVedioPlayer] clickedCloseButton ");
-    
+
     if (kyvedioPlayer.isFullscreen == YES) { //点击全屏模式下的关闭按钮
         self.navigationController.navigationBarHidden = NO;
         [self showCellCurrentVedioPlayer];
     }else{
-        
+
         [self closeCurrentCellVedioPlayer];
     }
 
@@ -367,7 +367,7 @@
 //点击全屏按钮代理方法
 -(void)kyvedioPlayer:(KYVedioPlayer *)kyvedioPlayer clickedFullScreenButton:(UIButton *)fullScreenBtn{
     NSLog(@"[KYVedioPlayer] clickedFullScreenButton ");
-    
+
     if (fullScreenBtn.isSelected) {//全屏显示
         self.navigationController.navigationBarHidden = YES;
         kyvedioPlayer.isFullscreen = YES;
@@ -376,20 +376,20 @@
     }else{
          self.navigationController.navigationBarHidden = NO;
         [self showCellCurrentVedioPlayer];
-        
+
     }
 
-    
-    
+
+
 }
 //单击WMPlayer的代理方法
 -(void)kyvedioPlayer:(KYVedioPlayer *)kyvedioPlayer singleTaped:(UITapGestureRecognizer *)singleTap{
-    
+
     NSLog(@"[KYVedioPlayer] singleTaped ");
 }
 //双击WMPlayer的代理方法
 -(void)kyvedioPlayer:(KYVedioPlayer *)kyvedioPlayer doubleTaped:(UITapGestureRecognizer *)doubleTap{
-    
+
     NSLog(@"[KYVedioPlayer] doubleTaped ");
 }
 
@@ -400,17 +400,17 @@
 }
 //准备播放的代理方法
 -(void)kyvedioPlayerReadyToPlay:(KYVedioPlayer *)kyvedioPlayer playerStatus:(KYVedioPlayerState)state{
-    
+
     NSLog(@"[KYVedioPlayer] kyvedioPlayerReadyToPlay  准备播放");
 }
 //播放完毕的代理方法
 -(void)kyplayerFinishedPlay:(KYVedioPlayer *)kyvedioPlayer{
-    
+
     NSLog(@"[KYVedioPlayer] kyvedioPlayerReadyToPlay  播放完毕");
-    
+
     [self closeCurrentCellVedioPlayer];
 
-    
+
 }
 
 
--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 # KYVedioPlayer
+
 KYVedioPlayer 是基于AVPlayer的封装视频播放器,支持播放mp4、m3u8、3gp、mov等格式；支持网络视频和本地视频播放；支持全屏和小屏幕播放；还在UITableViewCell中播放视频 ；支持横屏竖屏自动播放。
 
 ![](https://github.com/kingly09/KYVedioPlayer/blob/master/vedio01.gif)
 
 
-## 环境设置
-- 项目环境
-- Xcode 7.3.1
-- iOS 7.0 +
+# 安装
 
-- 使用 cocoaPods 管理第三方库， 如果电脑没有安装 cocoapods，请先安装 cocoapods。安装方式可参考：[最新版 CocoaPods 的安装流程](http://blog.sina.com.cn/s/blog_6ff6523d0102x8dq.html)
+### 要求
 
-- 项目中使用到的第三方库
+* Xcode 7 +
+* iOS 7.0 +
+
+KYVedioPlayer播放器的布局依赖`Masonry`框架，注意工程是否包含`Masonry`库，如果没有的话，可以使用CocoaPods安装`Masonry`库。
 
 ```
-    platform:ios,'7.0'
-    target "KYVedioPlayer" do
-    pod 'Masonry', '~> 1.0.1'        ## 轻量级的布局框架 
-    pod 'MJRefresh', '~> 3.1.12'     ## 上拉刷新和下拉刷新
-    pod 'SDWebImage', '~> 3.8.2'     ## 支持异步的图片下载+缓存
-    pod 'MBProgressHUD', '~> 1.0.0'  ## 提示框提示框
-    end
+	pod 'Masonry', '~> 1.0.1'
+```
 
+### 手动安装
+
+下载DEMO后,将子文件夹 **KYVedioPlayerLib** 拖入到项目中, 导入头文件`KYVedioPlayer.h` 开始使用.
+
+### CocoaPods安装
+
+你可以在 **Podfile** 中加入下面一行代码来使用 KYAlertView
+
+```
+	pod 'KYVedioPlayer'
 ```
 
 ## 实现的功能
@@ -32,6 +38,254 @@ KYVedioPlayer 是基于AVPlayer的封装视频播放器,支持播放mp4、m3u8�
 4. 支持UITableViewCell中播放视频；
 5. 支持横屏竖屏自动播放；
 
+# 如何使用
 
-大家若喜欢，给我一个 star 😊 ！ 十分感谢！🙏
+### 基本功能
 
+### step 1 :创建控制器
+
+在您需要使用`KYVedioPlayer`播放器功能的类中，import 头文件`KYVedioPlayer.h`即可 。
+设置 `KYVedioPlayerDelegate`委托代理  代码示例如下：
+
+```
+#import "KYVedioPlayer.h"
+
+@interface KYLocalVideoPlayVC ()<KYVedioPlayerDelegate>{
+    KYVedioPlayer  *vedioPlayer;
+    CGRect     playerFrame;
+}
+```
+
+### step 2 :初始化
+
+初始化VedioPlayer 初始化需要几个步骤：
+
+* 准备需要视频播放的UIView；
+* 新建player；
+* 设置url；
+* 调用 ` [vedioPlayer play] ` 开始播放。还可以设置播放器进度条的颜色，关闭按钮是否显示，视频标题等。
+
+
+新建一个 化VedioPlayer 播放器，代码示例如下：
+
+```
+    playerFrame = CGRectMake(0, 0, kScreenWidth, (kScreenWidth)*(0.75));
+   vedioPlayer = [[KYVedioPlayer alloc]initWithFrame:playerFrame];
+   vedioPlayer.delegate = self;
+   vedioPlayer.URLString = _URLString;
+   vedioPlayer.titleLabel.text = self.title;
+   vedioPlayer.closeBtn.hidden = NO;
+   vedioPlayer.progressColor = [UIColor orangeColor];
+   [self.view addSubview:vedioPlayer];
+
+```
+
+###  step 3 :启动播放器和暂停
+
+启动
+
+```
+[vedioPlayer play];    
+```
+
+暂停
+
+```
+[vedioPlayer pause];    
+```
+
+### step 4 :设置监听,屏幕旋转的通知，代码示例如下：
+
+```
+[[NSNotificationCenter defaultCenter] addObserver:self
+                                         selector:@selector(NotificationDeviceOrientationChange:)
+                                             name:UIDeviceOrientationDidChangeNotification
+                                           object:nil
+ ];
+
+ ```
+
+根据屏幕旋转的通知,是否全屏，是否缩小，代码示例如下：
+
+ ```
+ -(void)NotificationDeviceOrientationChange:(NSNotification *)notification{
+
+     if (vedioPlayer == nil|| vedioPlayer.superview==nil){
+         return;
+     }
+
+     UIDeviceOrientation orientation = [UIDevice currentDevice].orientation;
+     UIInterfaceOrientation interfaceOrientation = (UIInterfaceOrientation)orientation;
+     switch (interfaceOrientation) {
+         case UIInterfaceOrientationPortraitUpsideDown:{
+             NSLog(@"第3个旋转方向---电池栏在下");
+         }
+             break;
+         case UIInterfaceOrientationPortrait:{
+             NSLog(@"第0个旋转方向---电池栏在上");
+             if (vedioPlayer.isFullscreen) {
+                 [self setNeedsStatusBarAppearanceUpdate];
+                 [vedioPlayer showSmallScreenWithPlayer:vedioPlayer withFatherView:self.view withFrame:playerFrame];
+
+             }
+         }
+             break;
+         case UIInterfaceOrientationLandscapeLeft:{
+             NSLog(@"第2个旋转方向---电池栏在左");
+             vedioPlayer.isFullscreen = YES;
+             [self setNeedsStatusBarAppearanceUpdate];
+             [vedioPlayer showFullScreenWithInterfaceOrientation:interfaceOrientation player:vedioPlayer withFatherView:self.view];
+         }
+             break;
+         case UIInterfaceOrientationLandscapeRight:{
+             NSLog(@"第1个旋转方向---电池栏在右");
+             vedioPlayer.isFullscreen = YES;
+             [self setNeedsStatusBarAppearanceUpdate];
+             [vedioPlayer showFullScreenWithInterfaceOrientation:interfaceOrientation player:vedioPlayer withFatherView:self.view];
+         }
+             break;
+         default:
+             break;
+     }
+
+ }
+
+ ```
+
+
+### step 4 :注销播放器，代码示例如下：
+
+```
+ /**
+  *  注销播放器
+  **/
+ - (void)releasePlayer
+ {
+     [vedioPlayer resetKYVedioPlayer];
+      vedioPlayer = nil;
+ }
+
+ - (void)dealloc
+ {
+     [self releasePlayer];
+     [[NSNotificationCenter defaultCenter] removeObserver:self];
+     NSLog(@"KYLocalVideoPlayVC deallco");
+ }
+```
+
+### 其它操作，如隐藏状态栏，设置播放器进度条的颜色，关闭按钮是否显示，获取正在播放的时间点等，代码示例如下：
+
+* 隐藏状态栏
+
+```
+/**
+ *  隐藏状态栏
+ **/
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
+```
+
+* 设置播放器进度条的颜色
+
+```
+vedioPlayer.progressColor = [UIColor orangeColor];
+```
+
+* 关闭按钮是否显示，NO为显示关闭按钮 ，YES为隐藏关闭按钮
+
+```
+ vedioPlayer.closeBtn.hidden = NO;
+```
+
+* 获取正在播放的时间点
+
+```
+[vedioPlayer currentTime];
+```
+
+
+### 播放器事件
+
+播放器的几种状态，为`KYVedioPlayerState`枚举类型
+
+事件ID                    |     含义说明
+------------------------- | ---------------------
+KYVedioPlayerStateFailed    | 播放失败
+KYVedioPlayerStateBuffering | 缓冲中
+KYVedioPlayerStatusReadyToPlay | 将要播放
+KYVedioPlayerStatePlaying   | 播放中
+KYVedioPlayerStateStopped   | 暂停播放
+KYVedioPlayerStateFinished  | 播放完毕
+
+### 播放器回调
+
+播放器所有点击事件的回调都会通过这个`KYVedioPlayerDelegate`反馈给您的App.
+
+*
+
+
+### 更多播放器方法
+
+* 重置播放器
+
+```
+[vedioPlayer resetKYVedioPlayer];
+```
+
+* 设置全屏显示播放
+
+```
+/**
+ *  全屏显示播放
+ ＊ @param interfaceOrientation 方向
+ ＊ @param player 当前播放器
+ ＊ @param fatherView 当前父视图
+ **/
+-(void)showFullScreenWithInterfaceOrientation:(UIInterfaceOrientation )interfaceOrientation player:(KYVedioPlayer *)player withFatherView:(UIView *)fatherView;
+```
+
+* 设置小屏幕显示播放
+
+```
+/**
+ *  小屏幕显示播放
+ ＊ @param player 当前播放器
+ ＊ @param fatherView 当前父视图
+ ＊ @param playerFrame 小屏幕的Frame
+ **/
+-(void)showSmallScreenWithPlayer:(KYVedioPlayer *)player withFatherView:(UIView *)fatherView withFrame:(CGRect )playerFrame;
+
+```
+
+# 高级功能演示
+
+* 支持UITableViewCell中播放视频；
+
+* 记住上次播放的位置；
+
+
+
+#  联系与建议反馈
+
+>
+> **weibo:** [http://weibo.com/balenn](http://weibo.com/balenn)
+>
+> **QQ:** 362108564
+>
+
+如果有任何你觉得不对的地方，或有更好的建议，以上联系都可以联系我。 十分感谢！
+
+## 感谢
+
+KYVedioPlayer播放器的布局依赖`Masonry`框架，十分感谢`Masonry`开发人员对开源事业作出的贡献！
+
+# 鼓励
+
+它若不慎给您帮助，请不吝啬给它点一个**star**，是对它的最好支持，非常感谢！🙏
+
+
+# LICENSE
+
+KYAlertView 被许可在 **MIT** 协议下使用。查阅 **LICENSE** 文件来获得更多信息。
