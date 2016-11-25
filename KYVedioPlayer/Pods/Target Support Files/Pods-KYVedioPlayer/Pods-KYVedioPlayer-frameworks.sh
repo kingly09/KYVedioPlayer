ac275048119c8e65e1d60db611c2f10f22e2a452
kingly <kingly09@gmail.com>
2016-11-26 00:41:44 +0800

    update pod  cocoapods 1.1.0.rc.2

--- KYVedioPlayer/Pods/Target Support Files/Pods-KYVedioPlayer/Pods-KYVedioPlayer-frameworks.sh
+++ KYVedioPlayer/Pods/Target Support Files/Pods-KYVedioPlayer/Pods-KYVedioPlayer-frameworks.sh
@@ -82,3 +82,16 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+fi
